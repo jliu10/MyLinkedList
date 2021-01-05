1529be55b2221ee58b718b0a5726c94ab4fd14a1
Justin Liu <exovitejl@gmail.com>
2021-01-05 14:26:05 -0500

    wrote get/set methods for prev

--- Node.java
+++ Node.java
@@ -1,19 +1,29 @@
 public class Node{
- private String data;
- private Node next,prev;
- public Node(String value){
-   data=value;
-   next=null;
-   prev=null;
- }
+  private String data;
+  private Node next,prev;
+  public Node(String value){
+    data=value;
+    next=null;
+    prev=null;
+  }
 
- public String getData(){
+  public String getData(){
    return data;
- }
+  }
 
- public String setData(String s){
-   String old=data;
-   data=s;
-   return old;
- }
+  public String setData(String s){
+    String old=data;
+    data=s;
+    return old;
+  }
+
+  public Node getPrev(){
+    return prev;
+  }
+
+  public Node setPrev(Node s){
+    Node old=prev;
+    prev=s;
+    return old;
+  }
 }
