537c24ec847984d40e0e6ce2d998ecdb0a824753
Justin Liu <exovitejl@gmail.com>
2021-01-06 15:09:35 -0500

    wrote method add, developed method add(index)

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,11 +7,51 @@ public class MyLinkedList{
     end=null;
   }
 
-  public int size();
-  public boolean add(String value);
-  public boolean add(int index, String value);
-  public String get(int index);
-  public String set(int index, String value);
-  public String toString();
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String value){
+    Node newNode=new Node(value);
+    if(size()==0) start=newNode; //ADDING TO EMPTY
+    else{ //ADDING TO TAIL
+      end.setNext(newNode);
+      newNode.setPrev(end);
+    }
+    end=newNode;
+    size++;
+    return true;
+  }
+
+  public boolean add(int index, String value){
+    Node newNode=new Node(value);
+    if(index<0||index>size()) throw new IndexOutOfBoundsException();
+    else if(index==size()){
+      if(size()==0) start=newNode; //ADDING TO EMPTY
+      else{ //ADDING TO TAIL
+        end.setNext(newNode);
+        newNode.setPrev(end);
+      }
+      end=newNode;
+    }else if(index==0){ //ADDING TO HEAD
+      start.setPrev(newNode);
+      newNode.setNext(start);
+      start=newNode;
+    }else{ //ADDING TO MIDDLE
+      
+    }
+    size++;
+    return true;
+  }
+
+  public String get(int index){
+    return "";
+  }
+  public String set(int index, String value){
+    return "";
+  }
+  public String toString(){
+    return "";
+  }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
