505ca4decb91abcfeb47eff3eccc4168d77d3833
Justin Liu <exovitejl@gmail.com>
2021-01-06 16:53:31 -0500

    tested stuff

--- Tester.java
+++ Tester.java
@@ -1,12 +1,41 @@
 public class Tester{
 
   public static void main(String[]args){
-    Node a=new Node("apple");
+    try{
+      Node a=new Node("apple");
 
-    System.out.println(a.getData());
-    System.out.println(a.getPrev());
-    System.out.println(a.getNext());
-    a.setData("slapple");
-    System.out.println(a.getData());
+      System.out.println("-----TESTING NODE-----");
+      System.out.println(a.getData());
+      System.out.println(a.getPrev());
+      System.out.println(a.getNext());
+      a.setData("slapple");
+      System.out.println(a.getData());
+      System.out.println();
+
+      MyLinkedList x=new MyLinkedList();
+
+      System.out.println("-----TESTING MYLINKEDLIST-----");
+      System.out.println(x.size());
+      System.out.println(x);
+      x.add(a.getData());
+      System.out.println(x.size());
+      x.add("banana");
+      System.out.println(x.size());
+      x.add(1,"cat");
+      System.out.println(x.size());
+      x.add(0,"dog");
+      System.out.println(x.size());
+      System.out.println(x.get(0));
+      System.out.println(x.get(1));
+      System.out.println(x.get(2));
+      System.out.println(x.get(3));
+      System.out.println(x);
+      x.set(3,"bonkers");
+      System.out.println(x.get(3));
+      System.out.println(x);
+
+    }catch(IndexOutOfBoundsException e){
+      System.out.println("Index below 0 or above size");
+    }
   }
 }
